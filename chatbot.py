e06cd3d739cfcc0e94a7e1d80f11642a9665876a
El Mehdi Benennia <elmehdibenennia@gmail.com>
2024-01-13 04:50:03 +0100

    init

---
+++ chatbot.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from openai import OpenAI
+from langchain.agents import AgentExecutor
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.agents.openai_assistant import OpenAIAssistantRunnable
+
+# Set Streamlit title
+st.title("Datawise Bot")
+
+# Initialize OpenAI client with your API key
+client = OpenAI(api_key="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")
+
+# Initialize OpenAI Assistant
+assistant_id = "asst_X6RcOFD2WgbBuJoUFM7y2JpU"
+tools = [DuckDuckGoSearchRun()]
+agent = OpenAIAssistantRunnable(assistant_id="asst_X6RcOFD2WgbBuJoUFM7y2JpU", as_agent=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+# Initialize messages
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Input for a new message
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    # Get response from OpenAI Assistant
+    response = agent_executor.invoke({"content": prompt})['output']
+    
+    # Display assistant's response
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    
+    st.session_state.messages.append({"role": "assistant", "content": response})
